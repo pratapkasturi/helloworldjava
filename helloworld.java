7d12a2bc62d803eeedb1dd217befb4cff583b34f
pratapkasturi <59948680+pratapkasturi@users.noreply.github.com>
2022-05-05 13:27:45 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld {
    public static void main(String[] args) {
     System.out.println("Hello World from DevOps3");
+    System.out.println("Hello World from from Pratap Kasturi Github");
     }
 }
